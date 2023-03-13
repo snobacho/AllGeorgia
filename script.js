76ec3a98fda51e84915ca2eaa7372b5b36aa8453
snobacho <giorgicheli@gmail.com>
2023-03-13 16:33:39 +0400

    add tours animation cards

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const cardHeadPara = document.querySelector('.tours-heading-txt')
 window.addEventListener('scroll', function() {
     let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollPosition >= 1000) {
+    if (scrollPosition >= 1300) {
         document.querySelector('.tours-head').style.display = 'block';
+        cardHeadPara.style.animationName = "appear";
+        cardHeadPara.style.animationPlayState = "running";  
     }
-});
+});   // * card header appears when the card section is visible and an animation is starting
   
