b049096fd46a300bd9e6f5dd18eefe995181763b
snobacho <giorgicheli@gmail.com>
2023-03-25 23:20:23 +0400

    Add modal for order tours

--- all-tours.js
+++ all-tours.js
@@ -1,8 +1,7 @@
 const modal = document.querySelector('.modal');
-const modalBtns = document.querySelectorAll('.modal-btn');
-const closeBtn = document.querySelector('.close');
+const modalBtns = document.querySelectorAll('.tour-order-btn');
+const closeBtn = document.querySelector('.close-modal-btn');
 
-// Open modal when button is clicked
 modalBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -13,40 +12,8 @@ closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
-
 window.addEventListener('click', (event) => {
   if (event.target === modal) {
     modal.style.display = 'none';
   }
 });
-
-
-
-  /* .modal {
-    display: none;
-    position: fixed;
-    z-index: 1;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    overflow: auto;
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-  
-  .modal-content {
-    background-color: #fff;
-    margin: 10% auto;
-    padding: 20px;
-    border-radius: 5px;
-    width: 80%;
-    max-width: 600px;
-    position: relative;
-  }
-  
-  .close {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    font-size: 1rem;
-} */
